9efb1b7d9c866851a00164a5aa2fb840e7f68172
明远湖之鱼 <488548816@qq.com>
2024-12-04 00:19:04 +0800

    feat: 模版配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	sassOptions: {
+		api: 'modern-compiler',
+		additionalData: '@import "@/assets/styles/scss/index.scss";'
+	}
 };
 
 export default nextConfig;
